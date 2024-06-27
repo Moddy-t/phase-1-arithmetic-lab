cb7c66b4db51c1a45cd46163a4c8a74a902923de
Moddy-t <amalembamodesta@gmail.com>
2024-06-27 13:33:18 +0300

    completed arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const num1 = 31;
+const num2 = 2; 
+let multiply = num1 * num2;
+console.log(multiply);
 
+const random = Math.floor(Math.random() * 10)+1;
+
+
+const num3 = 22;
+const num4 = 9;
+const mod = num3%num4
+console.log(mod)
+
+Math.max(4, 14, 18, 20);
+const max = 20;
 // Write your code here
